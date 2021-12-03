be5a9fadf689ed2dfc2d3266e4e44c0b89bfe439
Maciej Majewski <maciej.w.majewski@gmai.com>
2021-12-03 06:43:55 +0100

    fixes

--- 03/t.py
+++ 03/t.py
@@ -7,26 +7,22 @@ def p1():
         a = [int(i) for i in d.split()[0]]
         dar.append(a)
     ar = np.array(dar)
-    bitnum1 = []
-    for i in range(ar.shape[1]):
-        bitnum1.append(1 if sum(ar[:,i])> ar.shape[0]//2 else 0)
+    bitnum1 = ar.sum(axis=0) >= ar.shape[0]/2
+    bitnum1 = list(bitnum1.astype(int))
     st = [str(i) for i in bitnum1]
     st2 = [str((not i)*1) for i in bitnum1]
     b1 = int("".join(st),2)
     b2 = int("".join(st2),2)
     print(b1, b2, b1*b2)
-    res = ...
-    print(res)
 
 def get_most_common(ar, i):
-    val = 1 if sum(ar[:,i])>= ar.shape[0]/2 else 0
+    val = 1 * (sum(ar[:,i])>= ar.shape[0]/2)
     return ar[ar[:,i] == val]
 
 
 def get_least_common(ar, i):
-    val = 1 if sum(ar[:,i])>= ar.shape[0]/2 else 0
-    val = 0 if val == 1 else 1
-    return ar[ar[:,i] == val]
+    val = 1 * (sum(ar[:,i])>= ar.shape[0]/2)
+    return ar[ar[:,i] != val]
 
 
 def p2():
@@ -40,15 +36,13 @@ def p2():
     tmpar = ar.copy()
     for i in range(ar.shape[1]):
         tmpar =  get_most_common(tmpar, i)
-        if len(tmpar[0]) == 1:
+        if len(tmpar) == 1:
             break
     st = [str(i) for i in tmpar[0]]
     b1 = int("".join(st),2)
-
     tmpar = ar.copy()
     for i in range(ar.shape[1]):
         tmpar =  get_least_common(tmpar, i)
-        print(len(tmpar))
         if len(tmpar) == 1:
             break
     st2 = [str(i) for i in tmpar[0]]
@@ -56,5 +50,5 @@ def p2():
     print(b1, b2, b1*b2)
 
 if __name__=="__main__":
-    # p1()
+    p1()
     p2()
